c5599ebf28fcfc36900c75b9b59048ba2afa3c58
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-07-31 16:35:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['6283833694338']
+global.premium = ['6283833694338']
+global.ownernomer = '6283833694338'
 global.ownername = 'Dark Alpha'
 global.botname = 'Bixby MD'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
